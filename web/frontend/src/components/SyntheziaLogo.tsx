862f7eb6fd3c827a0129a121185f30e465bb45a8
MatVD <vandammemathieu@hotmail.fr>
2025-11-21 07:03:37 +0100

    feat: update SyntheziaLogo component to use responsive logos and handle screen resizing

--- web/frontend/src/components/SyntheziaLogo.tsx
+++ web/frontend/src/components/SyntheziaLogo.tsx
@@ -1,11 +1,29 @@
-import logo from "../assets/synthezia-black.png"
+import synthezia_logo from "../assets/synthezia-black.png"
+import synthezia_thumb from "../assets/synthezia-black-logo.png"
+import { useEffect, useState } from "react";
+
 
 export function SyntheziaLogo({ className = "", onClick }: { className?: string; onClick?: () => void }) {
   const clickable = typeof onClick === 'function'
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 640)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 640)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+
   return (
     <div className={`${className}`}>
       <img
-        src={logo}
+      
+        src={isSmallScreen ? synthezia_thumb : synthezia_logo}
         alt="SynthezIA Logo"
         className={`h-8 sm:h-10 w-auto select-none ${clickable ? 'cursor-pointer hover:opacity-90 focus:opacity-90 outline-none' : ''}`}
         role={clickable ? 'button' as const : undefined}
